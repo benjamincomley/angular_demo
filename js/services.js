bdcd693eede7ae4ae5926f7c54658ade1f714357
Benji <benjamincomley@gmail.com>
2017-05-15 15:17:00 +0200

    put components in to IIFEs

--- js/services.js
+++ js/services.js
@@ -1,15 +1,17 @@
-angular.module("demoApp").service('userService', function(){
-  var collection = [
-    {firstName: 'Ben', lastName: 'Comley'},
-    {firstName: 'Claes', lastName: 'Enlund'},
-    {firstName: 'Andy', lastName: 'Carnahan'}
-  ];
-  return {
-    users: function(){
-      return collection;
-    },
-    add: function(object){
-      collection.push(object);
+(function () {
+  angular.module("demoApp").service('userService', function(){
+    var collection = [
+      {firstName: 'Ben', lastName: 'Comley'},
+      {firstName: 'Claes', lastName: 'Enlund'},
+      {firstName: 'Andy', lastName: 'Carnahan'}
+    ];
+    return {
+      users: function(){
+        return collection;
+      },
+      add: function(object){
+        collection.push(object);
+      }
     }
-  }
-});
+  });
+})();
