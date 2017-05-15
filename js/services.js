b0d86bb575bf2b1314de6bd57adb5017c18af2bc
Benji <benjamincomley@gmail.com>
2017-05-15 15:25:12 +0200

    add 'use strict' directive

--- js/services.js
+++ js/services.js
@@ -1,17 +1,21 @@
 (function () {
-  angular.module("demoApp").service('userService', function(){
-    var collection = [
-      {firstName: 'Ben', lastName: 'Comley'},
-      {firstName: 'Claes', lastName: 'Enlund'},
-      {firstName: 'Andy', lastName: 'Carnahan'}
-    ];
-    return {
-      users: function(){
-        return collection;
-      },
-      add: function(object){
-        collection.push(object);
+  'use strict';
+  angular
+    .module("demoApp")
+    .service('userService', function() {
+      var collection = [
+        {firstName: 'Ben', lastName: 'Comley'},
+        {firstName: 'Claes', lastName: 'Enlund'},
+        {firstName: 'Andy', lastName: 'Carnahan'}
+      ];
+
+      return {
+        users: function() {
+          return collection;
+        },
+        add: function(object) {
+          collection.push(object);
+        }
       }
-    }
-  });
+    });
 })();
