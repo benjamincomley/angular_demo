7e48338342ffb35351612870a36c3de1d44979a2
Benji <benjamincomley@gmail.com>
2017-05-15 15:41:17 +0200

    add named functions

--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,14 @@
   'use strict';
   angular
     .module("demoApp")
-    .controller("mainController", function($scope, userService) {
-      $scope.users = userService.users();
-      $scope.newUser = {};
-      $scope.addUser = function() {
-        userService.add($scope.newUser);
-        $scope.newUser = {}
-      }
-    });
+    .controller("mainController", mainController);
+
+  function mainController($scope, userService) {
+    $scope.users = userService.users();
+    $scope.newUser = {};
+    $scope.addUser = function() {
+      userService.add($scope.newUser);
+      $scope.newUser = {}
+    };
+  };
 })();
--- js/services.js
+++ js/services.js
@@ -2,20 +2,22 @@
   'use strict';
   angular
     .module("demoApp")
-    .service('userService', function() {
-      var collection = [
-        {firstName: 'Ben', lastName: 'Comley'},
-        {firstName: 'Claes', lastName: 'Enlund'},
-        {firstName: 'Andy', lastName: 'Carnahan'}
-      ];
+    .service('userService', userService);
 
-      return {
-        users: function() {
-          return collection;
-        },
-        add: function(object) {
-          collection.push(object);
-        }
+  function userService() {
+    var collection = [
+      {firstName: 'Ben', lastName: 'Comley'},
+      {firstName: 'Claes', lastName: 'Enlund'},
+      {firstName: 'Andy', lastName: 'Carnahan'}
+    ];
+
+    return {
+      users: function() {
+        return collection;
+      },
+      add: function(object) {
+        collection.push(object);
       }
-    });
+    }
+  };
 })();
